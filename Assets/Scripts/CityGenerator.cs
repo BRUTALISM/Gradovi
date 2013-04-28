2aacf993cb17b0ee9834b7c7078961d171445639
Vladimir Mitrovic <vladimirm@nordeus.eu>
2013-04-28 21:34:47 +0200

    Setting up...

---
+++ Assets/Scripts/CityGenerator.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class CityGenerator : MonoBehaviour {
+	
+	void Start() {
+		
+	}
+	
+	void Update() {
+		if (Application.isPlaying) {
+			// Play mode
+		} else {
+			// Edit mode
+		}
+	}
+}
