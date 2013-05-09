4f086d68b8c8a1d6528f7c5181d2c59ddf0dadb5
Vladimir Mitrovic <vladimirm@nordeus.eu>
2013-05-09 23:24:04 +0200

    Messing around with population densities.

---
+++ Assets/Scripts/PopulationDensity.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using System.Collections;
+
+public enum DensityFalloff {
+	Linear,
+	Quadratic,
+	Exponential
+}
+
+public class PopulationDensity : MonoBehaviour {
+	public float radius = 1000f;
+	public float densityAtCenter = 100;
+	public DensityFalloff densityFalloff = DensityFalloff.Linear;
+	
+	void OnDrawGizmos() {
+		Gizmos.DrawWireSphere(transform.position, radius);
+	}
+	
+	public float DensityAt(Vector3 worldPosition) {
+		switch (densityFalloff) {
+		case DensityFalloff.Linear:
+			return Mathf.Clamp((transform.InverseTransformPoint(worldPosition) - transform.position).magnitude / radius, 0f, 1f) * densityAtCenter;
+		case DensityFalloff.Quadratic:
+			// FIXME: Implement.
+			return 1f;
+		case DensityFalloff.Exponential:
+			// FIXME: Implement.
+			return 1f;
+		}
+	}
+}
